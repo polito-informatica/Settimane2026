9a7a334d4484b20867387edd08237b19c1dced70
Fulvio Corno <fulvio.corno@polito.it>
2026-09-21 23:23:28 +0200

    Esercizio L01

--- Settimana01/esercizio.md
+++ Settimana01/esercizio.md
@@ -4,7 +4,41 @@ Quali sono i nomi più frequenti in questa aula?
 
 # Analisi
 
+Consideriamo il nome proprio di ciascuno
+Nel caso di nomi multipli, li separo e calcolo separatamenti
+
+Troviamo i 10 nomi più frequenti
+Stampiamo l'elenco di questi 10, in ordine
+decrescente di frequenza
+A fianco di ciascun nome stampiamo ache il valore 
+della frequenza
+In caso di nomi a pari merito mi fermo comunque a 10.
+
+Cambio problema: Quali sono i nomi più frequenti tra gli iscritti all'insegnamento?
 
 # Progettazione / Design
+
+1A)
+Preparo una tabella in cui segnarmi i nomi già visti
+COnsidero una per volta le righe dell'elenco
+Verifico se quel nome è già presente nella tabella
+Se non è ancora presente, allora cerco nell'enenco quanti hanno quel nome
+Scrivo nella tabella tale informazione
+
+1B)
+Preparo una tabella in cui segnarmi i nomi 
+Considero una per volta le righe dell'elenco
+Se quel nome è composto, dividilo nelle sue componenti
+Verifico se quel nome c'è nella tabella
+Se non c'è, lo aggiungo con frequenza 1
+Se c'è correggo la frequenza aggiugendo 1
+
+1C)
+Metti il registro in ordine alfabetico per nome
+....
+
+2)
+Metto in ordine la tabellina per valori decrescenti della frequenza
+Stampo le prime 10 righe
 
 # Implementazione / Test
---
+++ Settimana01/nomi_frequenti.py
@@ -0,0 +1,56 @@
+# Quali sono i nomi più frequenti?
+
+# from collections import Counter
+import csv
+import operator
+
+
+ELENCO = "14BHDWZ_2027_shuffled.csv"
+NUM_RISULTATI = 20
+
+
+def main():
+    studenti = leggi_studenti(ELENCO)
+    nomi = [studente["NOME"] for studente in studenti]
+
+    # l'esercizio si ruidurrebbe a 2 righe se si utilizzasse la classe Counter
+    ## stat = Counter(nomi)
+    ## print(stat.most_common(NUM_RISULTATI))
+    # ma noi, nel seguito dell'esercizio, preferiamo procedere "a mano" per comprendere meglio i passaggi necessari
+
+    # opzionale: si potrebbero dividere i nomi multipli in più elementi distinti
+    # nomi_splittati = []
+    # for nome in nomi:
+    #     nomi_splittati.extend(nome.split())
+    # nomi = nomi_splittati
+
+    # per ogni nome, calcola quante volte compare
+    # costruisci un dizionario: chiave = nome(str), valore = numero di occorrenze (int)
+    frequenze = dict()
+    for nome in nomi:
+        frequenze[nome] = frequenze.get(nome, 0) + 1
+
+    # converti dizionario in lista di tuple. Ciascuna tupla è (nome, frequenza)
+    tabella_frequenze = list(frequenze.items())
+    # ordina la lista per frequenza descrescente
+    tabella_frequenze.sort(key=operator.itemgetter(1), reverse=True)
+
+    stampa_tabella(tabella_frequenze, NUM_RISULTATI)
+
+
+def leggi_studenti(nome_file):
+    with open(nome_file, "r", encoding="utf-8", newline="") as f:
+        reader = csv.DictReader(f)
+        elenco = list(reader)
+    return elenco
+
+
+def stampa_tabella(tabella_frequenze, num):
+    print("Nomi piu frequenti:")
+    i = 1
+    for nome, numero in tabella_frequenze[:num]:
+        print(f"{i:2d}. {nome:20s}: {numero:3d}")
+        i += 1
+
+
+main()
